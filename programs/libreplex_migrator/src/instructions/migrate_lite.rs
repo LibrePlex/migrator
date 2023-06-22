0399e4853e0feff097bd72415c94cfd4c664a40a
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2023-06-22 19:40:05 +0100

    Upgrade to metadata 0.3.0

--- programs/libreplex_migrator/src/instructions/migrate_lite.rs
+++ programs/libreplex_migrator/src/instructions/migrate_lite.rs
@@ -1,9 +1,12 @@
 
+use anchor_lang::accounts::signer;
 use anchor_lang::{prelude::*};
 use anchor_spl::token::Mint;
 use libreplex_metadata::cpi::accounts::GroupAdd;
 use libreplex_metadata::{Group, CreateMetadataInput, Asset};
 use libreplex_metadata::program::LibreplexMetadata as MetadataProgram;
+use crate::libreplex_migrator;
+use crate::program::LibreplexMigrator as LibrePlexMigrator;
 use libreplex_metadata::cpi::accounts::CreateMetadata;
 use mpl_token_metadata::state::{Metadata as LegacyMetadata, TokenMetadataAccount};
 
@@ -12,11 +15,9 @@ pub struct MigrateLite<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
 
-    #[account()]
-    pub root: Signer<'info>,
-
     #[account(mut)]
     pub group: Option<Account<'info, Group>>,
+
 
     #[account()]
     pub mint: Account<'info, Mint>,
@@ -31,6 +32,8 @@ pub struct MigrateLite<'info> {
 
     pub libreplex_metadata_program: Program<'info, MetadataProgram>,
 
+    pub libreplex_migrator_program: Program<'info, LibrePlexMigrator>,
+
     pub system_program: Program<'info, System>,
 }
 
@@ -44,6 +47,7 @@ pub fn handler(
     let payer = &ctx.accounts.payer;
     let group = &ctx.accounts.group;
     let system_program = &ctx.accounts.system_program;
+    let libreplex_migrator = &ctx.accounts.libreplex_migrator_program;
 
     let legacy_metadata_obj = LegacyMetadata::from_account_info(
         &legacy_metadata.to_account_info())?;
@@ -56,6 +60,11 @@ pub fn handler(
         return Err(ErrorCode::ConstraintMintMintAuthority.into());
     }
 
+    let mint_key = mint.key();
+
+    let signer_seeds = [
+        mint_key.as_ref()
+    ];
     /*
      create libre metadata, replicating what we can from legacy: name, symbol etc
      for asset, we use Asset::Json as this provides backwards compatible data 
@@ -63,21 +72,25 @@ pub fn handler(
      it's completely possible to switch to, say, inscriptions later if we want to.
     */
     libreplex_metadata::cpi::create_metadata(
-        CpiContext::new(
+        CpiContext::new_with_signer(
             libreplex_metadata.to_account_info(),
             CreateMetadata {
                 metadata: legacy_metadata.to_account_info(),
                 mint: mint.to_account_info(),
+                authority: payer.to_account_info(),
                 system_program: system_program.to_account_info(),
-                signer: payer.to_account_info()
-            }
+                signer: payer.to_account_info(),
+                invoked_migrator_program: Some(libreplex_migrator.to_account_info())
+            },
+            &[&signer_seeds]
         ),
         CreateMetadataInput {
             name: legacy_metadata_obj.data.name,
             symbol: legacy_metadata_obj.data.symbol,
             asset: Asset::Json { url: legacy_metadata_obj.data.uri },
             description: None,
-            update_authority: payer.key()
+            update_authority: payer.key(),
+            
         }
     )?;
 
